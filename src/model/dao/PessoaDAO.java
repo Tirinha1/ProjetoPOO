36f498c6e556206f2a13c767a4eafc155b4bddc7
Tirinha1 <gabrielssantos702@gmail.com>
2024-08-29 12:11:34 -0300

    primeiro DAO

--- src/model/dao/PessoaDAO.java
+++ src/model/dao/PessoaDAO.java
@@ -4,9 +4,69 @@
  */
 package model.dao;
 
+import java.time.LocalDate;
+import model.Pessoa;
+
 /**
  *
  * @author Gabriel
  */
 public class PessoaDAO {
+
+    private Pessoa[] pessoas;
+    private int count;
+
+    public PessoaDAO(int tamanho) {
+        pessoas = new Pessoa[tamanho];
+        count = 0;
+    }
+
+    public void criarPessoa(Pessoa pessoa) {
+        if (count < pessoas.length) {
+            pessoas[count] = pessoa;
+            count++;
+        } else {
+            System.out.println("capacidade maxima atingida");
+        }
+    }
+
+    public Pessoa getPessoaById(int id) {
+        for (int i = 0; i < count; i++) {
+            if (pessoas[i].getId() == id) {
+                return pessoas[i];
+            }
+        }
+        return null;
+    }
+
+    public void updatePessoa(Pessoa pessoa) {
+        for (int i = 0; i < count; i++) {
+            if (pessoas[i].getId() == pessoa.getId()) {
+                pessoas[i].updatePessoa(pessoa.getNome(), pessoa.getNascimento(), pessoa.getTelefone());
+                return;
+            }
+        }
+        System.out.println("Pessoa não encontrada");
+    }
+
+    public void deletePessoa(int id) {
+        for (int i = 0; i < count; i++) {
+            if (pessoas[i].getId() == id) {
+                for (int j = i; j < count - 1; j++) {
+                    pessoas[j] = pessoas[j + 1];
+                }
+                pessoas[count - 1] = null;
+                count--;
+                return;
+            }
+        }
+        
+        System.out.println("Pessoa não encontrada");
+    }
+    
+    public void getAllPessoa(){
+        for(int i = 0; i < count; i++){
+            System.out.println(pessoas[i].toString());
+        }
+    }
 }
